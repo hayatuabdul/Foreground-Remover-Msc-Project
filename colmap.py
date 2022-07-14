680d863352c411be463ba2517137b625ddc714e9
Hayatu Abdullahi <72262907+hayatuabdul@users.noreply.github.com>
2022-07-14 15:52:18 +0100

    Integrate main code with GUI
    The User Interface works well to grant Users control for accessing images and visualizing the point cloud. It also outputs the Camera Intrinsic parameters upon command.

--- colmap.py
+++ colmap.py
@@ -12,9 +12,23 @@ import open3d as o3d
 
 from read_write_model import read_model, read_next_bytes, read_cameras_text, read_cameras_binary, read_array, write_array
 
+from GUI import run, myClick
+
 
 # Read and manipulate depth data obtained from Colmap. The depth data can then be converted to a 3d point cloud visualized in open3d
 
+
+def calculate(x, y):
+    
+    z = x+y
+    #print('Camera Intrinsics:', fx,fy,cx,cy)
+    return z
+
+def cal(z):
+    
+    z = z+43
+    #print('Camera Intrinsics:', fx,fy,cx,cy)
+    return z
 
 
 def camera_intrinsic(camera):
@@ -23,7 +37,7 @@ def camera_intrinsic(camera):
     fy = float(((str(camera[1]).lstrip('[').rstrip(']'))))
     cx = float(((str(camera[2]).lstrip('[').rstrip(']'))))
     cy = float(((str(camera[3]).lstrip('[').rstrip(']'))))
-    #print(fx,fy,cx,cy)
+    #print('Camera Intrinsics:', fx,fy,cx,cy)
     return fx, fy, cx, cy
 
 
@@ -32,16 +46,17 @@ def convert_img(path, depth_map):
     rgb = Image.open(path)
     rgb2 = np.asarray(rgb)
     dp_size = depth_map.shape
-    img =cv2.resize(rgb2,(dp_size[1],dp_size[0]))
+    if rgb2.size != depth_map.size:
+        img =cv2.resize(rgb2,(dp_size[1],dp_size[0]))
+        img4 = Image.fromarray(img)
+        
+        if img4.mode != 'RGB':
+            img4 = img4.convert('RGB')
+        
+    print('Depth Size', depth_map.shape)
     print('Image Size',img.shape)
     
-    img4 = Image.fromarray(img)
-    
- 
-    if img4.mode != 'RGB':
-        img4 = img4.convert('RGB')
-        
-    return img4
+    return img4, rgb
 
 def convert_depth(depth_map):
     
@@ -62,7 +77,6 @@ def newDepth(path):
     
     for v in range(depth.shape[0]):
         for u in range(depth.shape[1]):
- 
             Z = depth[v, u] 
             Z2 = Z - 10
             
@@ -71,13 +85,6 @@ def newDepth(path):
     return depth2
        
     
-focalX = 1.52973780e+03
-focalY = 1.00800000e+03
-centerX = 7.56000000e+02
-centerY = 1.06462694e-02
-scalingFactor = 7
-L = 87
-H = 20
 # Convert Depth to Point cloud with only RGB range
 def getPointCloud(path, distance, fx, fy, cx, cy):
     thresh = 15.0
@@ -108,7 +115,6 @@ def getPointCloud(path, distance, fx, fy, cx, cy):
                 Y = (v - cx) * Z / fy
                 srcPxs.append((u, v))
                 points.append((X, Y, Z))
-  
             else:
                 pass
             
@@ -176,9 +182,9 @@ def custom_draw_geometry_with_custom_fov(pcd, fov_step):
     vis.add_geometry(pcd)
     view_pcd = vis.get_view_control()
     #view_pcd.set_front((0.1, 0.8, 1.3))
-    view_pcd.set_front((-0.15, 1.3, 1.3))
+    view_pcd.set_front((-0.15, 1.3, 2.2))
     view_pcd.set_up((0, 1, 0))
-    view_pcd.set_zoom(0.5)
+    view_pcd.set_zoom(0.45)
     view_pcd.change_field_of_view(step = fov_step)
     vis.run()
     vis.destroy_window()
@@ -188,11 +194,11 @@ def custom_draw_geometry_with_custom_fov(pcd, fov_step):
 def custom_draw_geometry_with_rotation(pcd, fov_step):
 
     def rotate_view(vis):
-        view_ctl = vis.get_view_control()
-        view_ctl.set_front((0, 0, 1))
-        view_ctl.set_up((0, 1, 0))
-        view_ctl.set_zoom(0.8)
-        view_ctl.change_field_of_view(step = fov_step)
+        view_pcd = vis.get_view_control()
+        view_pcd.set_front((0, 0, 1))
+        view_pcd.set_up((0, 1, 0))
+        view_pcd.set_zoom(0.8)
+        view_pcd.change_field_of_view(step = fov_step)
         #ctr.rotate(10.0, 0.0)
         return False
 
@@ -209,22 +215,32 @@ def main():
     if not os.path.exists('maps/normal_maps'):
         raise FileNotFoundError("File not found: {}".format('maps/depth_maps/0.jpeg.geometric'))
 
-    depth_map = read_array('Reconstruction/dense/0/stereo/depth_maps/11.jpeg.geometric.bin', ext = ".bin")
-    normal_map = read_array('maps/normal_maps/0.jpeg.geometric.bin', ext = ".bin")
+    depth_map = read_array('Reconstruction/dense/0/stereo/depth_maps/2.jpeg.geometric.bin', ext = ".bin")
+     # Resize RGB image to match the depth size for 3D color matching
+    img4, rgb = convert_img('images/2.jpeg', depth_map)
+    
+    #normal_map = read_array('maps/normal_maps/0.jpeg.geometric.bin', ext = ".bin")
     #min_depth, max_depth = np.percentile(depth_map, [5, 95])
     
-    camera = read_cameras_text('Reconstruction/sparse/0/cameras.txt')
+    #camera = read_cameras_text('Reconstruction/sparse/0/cameras.txt')
     
     # Extract camera intrinsic parameters automatically from the bin file
     camera_bin = read_cameras_binary('Reconstruction/sparse/0/cameras.bin')
     fx, fy, cx, cy = camera_intrinsic(camera_bin)
-
-    # Resize RGB image to match the depth size for 3D color matching
-    img4 = convert_img('images/11.jpeg', depth_map)
     
+    pic1 = Image.open('images/0.jpeg')
+    pic2 = Image.open('images/1.jpeg')
+    pic3 = Image.open('images/2.jpeg')
+    pic4 = Image.open('images/3.jpeg')
+    pic5 = Image.open('images/4.jpeg')
+    pic6 = Image.open('images/5.jpeg')
     
-    # This value decieds how deep the environment gets zoomed in
-    Depth = 7.2
+    #myClick(fx)
+    result = calculate(3,5)
+    res = cal(result)
+    #print(result)
+    # This value decides how deep the environment gets zoomed in
+    Depth = 0
 
     # Visualize the depth map.
     #plt.figure()
@@ -246,16 +262,15 @@ def main():
     
 
     # Display using Open3D visualization tools
-    #pcd_plot2.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     pcd_plot2.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     #o3d.visualization.draw_geometries([pcd_plot2])
     
     
-    custom_draw_geometry_with_custom_fov(pcd_plot2, 0)
+    #custom_draw_geometry_with_custom_fov(pcd_plot2, 0)
     #custom_draw_geometry_with_rotation(pcd_plot2, -90)
     
-   
-    
+    # Execute the Graphical User Interface
+    run(fx, fy, cx, cy, pcd_plot2, pic1, pic2, pic3, pic4, pic5, pic6)
     #write_array(depth_map, 'maps/depth2.pgm')
 
 
