16924731bbc1395eeeefdd1922085011c6e923d7
Hayatu Abdullahi <72262907+hayatuabdul@users.noreply.github.com>
2022-07-26 11:12:52 +0100

    Added Dense Manipulation
    Successfully accessed and manipulated the dense PLY file to modify the depth in the point cloud.

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from PIL import ImageTk, Image
 import cv2
 import os
 from os.path import splitext
+from pyntcloud import PyntCloud
 import ntpath
 import collections
 import struct
 import numpy as np
 import threading
 import matplotlib.pyplot as plt
-from PIL import Image
 import open3d as o3d
 
-from read_write_model import read_model, read_next_bytes, read_cameras_text, read_cameras_binary, read_array, write_array, read_cameras_binary, read_images_binary
+from read_write_model import read_model, read_next_bytes, read_cameras_text, read_cameras_binary, read_images_binary, read_array, write_array, qvec2rotmat
+
+
+
 
 # Initialize TKinter
 tk = Tk()
@@ -26,7 +29,7 @@ windowWidth = tk.winfo_reqwidth()
 windowHeight = tk.winfo_reqheight()
 positionRight = int(tk.winfo_screenwidth()/5 - windowWidth/5)
 positionDown = int(tk.winfo_screenheight()/5 - windowHeight/2)
-tk.geometry(f"1550x650+{positionRight}+{positionDown}")
+tk.geometry(f"1500x650+{positionRight}+{positionDown}")
 
 # Initialize frame
 my_frame = Frame(tk)
@@ -43,11 +46,11 @@ sl = Label(my_frame)
 sl.grid(row=1,column=2, sticky=W, padx="200")
 sl2 = Label(my_frame, text="Manipulate Depth", font="bold")
 #sl2.grid(row=1,column=2, sticky=W, padx="200")
-sl2.place(x=1254,y=210)
+sl2.place(x=1248,y=210)
 
 # Value for slider button
 value = IntVar() 
-    
+
 # Manually select RGB image
 def rgb_img():
     global tkimage2
@@ -72,6 +75,7 @@ def depth_map():
     global tkimage
     global depth_map
     global dp_img
+    global img
     global img_rgb
     global depthselect
     global imgselect
@@ -94,6 +98,7 @@ def depth_map():
     dp_img = dp_img.convert('RGB')
     dp_img.save("depth_img.png")
     img = Image.open("depth_img.png")
+    img = img.convert('RGB')
     img.thumbnail((560, 490))
     tkimage = ImageTk.PhotoImage(img)
     
@@ -146,6 +151,7 @@ def convert():
     global depth_map
     global img4
     global rgb
+    global rgb2
     
     rgb = Image.open(imgselect)
     rgb2 = np.asarray(rgb)
@@ -174,7 +180,6 @@ def point_cloud():
     depthz = depth_map
     points = []
     colors = []
-    srcPxs = []
     
     for v in range(depthz.shape[0]):
         for u in range(depthz.shape[1]):
@@ -184,14 +189,12 @@ def point_cloud():
             if (Z > depth):
                 X = (u - cx) * Z / fx
                 Y = (v - cy) * Z / fy
-                srcPxs.append((u, v))
                 points.append((X, Y, Z))
                 colors.append(rgb.getpixel((u, v)))
             else:
                 pass
             
 
-    srcPxs = np.asarray(srcPxs).T
     points = np.asarray(points)
     colors = np.asarray(colors)
 
@@ -213,9 +216,179 @@ def point_cloud():
     vis.destroy_window() 
         
     
+# Generate and visualize Dense model outputted by Colmap. Also access and manipulate depth pixels         
+def ply_cloud():
+    global ply_file
+    global point_cloud_in_numpy
+    global colors_cloud_in_numpy
+    global pcd2
+    global img_bin
+    global camera_bin2
+    global pointy
+    global coly
 
+    ply_file = ('Reconstruction/dense/0/fused.ply')
     
+
+    #images = img_bin
+    #cameras = camera_bin2
+    #im1 = images[3]
+    #print(im1)
+    #cam1 = cameras[1]
+    #print(cam1)
+    pcd2 = o3d.io.read_point_cloud(ply_file)
     
+    point_cloud = PyntCloud.from_file(ply_file)
+    xyz_arr = point_cloud.points.loc[:, ["x", "y", "z"]].to_numpy()
+    normal_arr = point_cloud.points.loc[:, ["nx", "ny", "nz"]].to_numpy()
+    color_arr = point_cloud.points.loc[:, ["red", "green", "blue"]].to_numpy()
+    #print(xyz_arr)
+    
+    pointy = xyz_arr
+    coly = color_arr
+    
+    points = []
+    colors = []
+    points2 = []
+    points3 = []
+    colors2 = []
+
+    # Individually access the columns from the numpy point cloud
+    points7 = pointy
+    coly7 = coly
+    colyx = coly7[:, 0:1]
+    colyy = coly7[:, 1:2]
+    colyz = coly7[:, 2:3]
+    
+    # Remove brackets and quotations marks
+    CX0 = colyx.flatten()
+    CY0 = colyy.flatten()
+    CZ0 = colyz.flatten()
+
+    print(colyx)
+    #print(coly.shape)
+    #print(points.shape)
+    pointyx = points7[:, 0:1]
+    #print(pointyx)
+    pointyy = points7[:, 1:2]
+    pointyz = points7[:, 2:3]
+    X0 = pointyx.flatten()
+    Y0 = pointyy.flatten()
+    Z0 = pointyz.flatten()
+    X = X0
+    Y = Y0
+    Z = Z0
+    #X = ((str(pointyx[0]).lstrip('[').rstrip(']')))
+    print('B4',pointyx)
+    print('Aff',X)
+    sizex = len(X)
+    print(sizex)
+    #sizey = len(Y)
+    #sizez = len(Z)
+    #print('Size of X', sizex)
+    #print('Size of Y', sizey)
+    #print('Size of Z', sizez)
+    #mask = np.abs(pointyz) > 5
+    #pointyz = pointyz(mask)
+    #points[:, 2:3] = np.abs(points[:, 2:3]) < 8
+    # Loop through all the points in the point cloud
+    for i in range(sizex):
+        X2 = X[i]
+        CX = CX0[i]
+        #X3 = str(X2).translate(translation)
+        Y2 = Y[i]
+        CY = CY0[i]
+        
+        Z2 = Z[i]
+        CZ = CZ0[i]
+            
+        if (Z2 > depth):
+            points.append((X2,Y2,Z2))
+            colors.append((CX,CY,CZ))
+        else:
+            pass
+            
+    points = np.asarray(points)
+    colors = np.asarray(colors)
+    #points2v = points[:, 0:3]
+    #points3 = np.asarray(points3)
+    #print(points2)
+    #print('After', points2.shape)
+    #print('Point3', points.shape)
+    #print(points2)
+    #print(points3)
+    print(colors)
+    pcd = o3d.geometry.PointCloud()
+
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(colors/255)
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    o3d.visualization.draw_geometries([pcd])
+    #view_pcd.set_front((0.1, 0.8, 1.3))
+    #vis = o3d.visualization.Visualizer()
+    #view_pcd = vis.get_view_control()
+    #view_pcd.set_front((-0.15, 1.3, 2.2))
+    #view_pcd.set_up((0, 1, 0))
+    #view_pcd.set_zoom(0.45)
+    
+    #view_pcd.change_field_of_view(step = fov_step)
+    #vis.run()
+    #vis.destroy_window() 
+    
+    #points.append((xyz_arr))
+    #points = np.asarray(points)
+    #pcd = o3d.geometry.PointCloud()
+    #pcd.xyz_arr = o3d.utility.Vector3dVector(xyz_arr)
+    
+    #o3d.visualization.draw_geometries([pcd2]) 
+    #point_cloud_in_numpy = np.asarray(pcd2.points) 
+    #colors_cloud_in_numpy = np.asarray(pcd2.colors) 
+
+
+
+def point_cloud3():
+    global depth_map
+    global fx, fy, cx, cy
+    """Transform a depth image into a point cloud with one point for each
+    pixel in the image, using the camera transform for a camera
+    centred at cx, cy with field of view fx, fy.
+
+    depth is a 2-D ndarray with shape (rows, cols) containing
+    depths from 1 to 254 inclusive. The result is a 3-D array with
+    shape (rows, cols, 3). Pixels with invalid depth in the input have
+    NaN for the z-coordinate in the result.
+
+    """
+    points = []
+    depth = depth_map
+    rows, cols = depth.shape
+    c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+    valid = (depth > 0) & (depth < 255)
+    z = np.where(valid, depth / 256.0, np.nan)
+    x = np.where(valid, z * (c - cx) / fx, 0)
+    y = np.where(valid, z * (r - cy) / fy, 0)
+    #points.append((x, y, z))
+    pcd3 = np.dstack((x, y, z))
+    #points = np.asarray(points)
+    #plt.figure()
+    #plt.imshow(pcd3)
+    #plt.show()
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    #data = np.random.random(size=(3, 3, 3))
+    z, x, y = pcd3.nonzero()
+    ax.scatter(x, y, z, c=z, alpha=1)
+    plt.show()
+    
+    #pcd3 = o3d.geometry.PointCloud()
+    #pcd3.points = o3d.utility.Vector3dVector(points)
+    #pcd3.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+    #3d.visualization.draw_geometries([pcd3])
+
+
 # Use threads to respond to button clicks    
 def target1():
     global img_bin
@@ -234,8 +407,12 @@ def target3():
     threading.Thread(target=lambda:camera_intrinsic(camera_bin)).start()
 def target4():
     threading.Thread(target=point_cloud).start()
-
-
+    #threading.Thread(target=convert_depth_pixel_to_point).start()
+def target5():
+    
+    threading.Thread(target=ply_cloud).start()
+    #threading.Thread(target=dep_to_ply).start()
+    
 
 B1 = Button(tk, text = "Choose Color Image", padx=20, pady=15, command=target1, relief="solid")
 B1.config(cursor="hand2")
@@ -257,6 +434,11 @@ B4.place(x=1255,y=380)
 B5 = Button(tk, text = "Activate Slider", padx=50, pady=30, command=target2, relief="solid")
 B5.config(cursor="hand2")
 B5.place(x=1251,y=530)
+
+B6 = Button(tk, text = "View Dense", padx=50, pady=30, command=target5, relief="solid")
+B6.config(cursor="hand2")
+B6.place(x=1051,y=530)
+
 
 
 tk.mainloop()
